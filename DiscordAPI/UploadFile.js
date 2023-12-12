451d9ff96ad230ac591172fa90917eff137f75c8
BhavyaMPatel <bhavyapatel4012@gmail.com>
2023-12-12 11:28:08 +0530

    UploadFile -2 Need To Do Changes

--- DiscordAPI/UploadFile.js
+++ DiscordAPI/UploadFile.js
@@ -3,35 +3,40 @@ import FilenameConvert from "../Formating/FilenameConvert.js";
 import GetFileData from "../Formating/GetFileData.js";
 import DataToDiscordFormat from "../Formating/DataToDiscordFormat.js";
 import 'dotenv/config'
+import { Message } from "discord.js";
 
 function UploadFile(FileName){
     console.log("Uploading file........");
 	client.on("ready",async ()=>{
-        console.log("IB");
+        /*ENV*/
+        const guild = client.guilds.cache.get(process.env.GUILD_ID);
+		const category = guild.channels.cache.get(process.env.CAT_ID);
+        console.log("ENV Verifty........");
+        
         /*Data Collection And Conversion*/
 		const FileData=GetFileData(`${FileName}`)
         const stringData = FileData.toString("hex");
         const discordFormat = DataToDiscordFormat(stringData);
+
         /*File Name*/
         const FileAName=FileName.split("/").pop();
         const FileChannelName=FilenameConvert(FileAName);
         console.log("Data Collection And Conversion Done ........");
-        /*ENV*/
-        const guild = client.guilds.cache.get(process.env.GUILD_ID);
-		const category = guild.channels.cache.get(process.env.CAT_ID);
-        console.log("ENV  ........");
+
         /*Check Compatibility*/
         const sameNameFiles = guild.channels.cache.find((channel) => channel.name === FilenameConvert(FileAName));
         if (sameNameFiles) {
             console.error(`File: ${FileName} already exists.`);
             return client.destroy();
         }
+
         console.log("Compatibility Done........");
 		const channel = await category.children.create({
             name: FileChannelName,
         });
+
         console.log("File Begin Upload ........");
-        
+
         const promises = discordFormat.map(async (message) => {
             await channel.send(message);
         });
@@ -39,6 +44,8 @@ function UploadFile(FileName){
         await Promise.all(promises);
         
         console.log(`File: ${FileName} has been uploaded.`);
+        client.destroy();
+        
 	})
 }
 
